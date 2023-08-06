365955ab532527d142900a63c0edea38197545e0
saraoriola <saraoriola@outlook.com>
2023-08-06 11:56:24 +0200

    updateCourse added

--- controllers/CourseController.js
+++ controllers/CourseController.js
@@ -21,6 +21,33 @@ const CourseController = {
         }
       },
 
+      async updateCourse(req, res) {
+        const courseId = req.params.id;
+        const { name, description, price, company, category } = req.body;
+    
+        try {
+          const course = await Course.findByPk(courseId);
+          if (!course) {
+            return res.status(404).json({ message: 'Course not found' });
+          }
+    
+          course.name = name;
+          course.description = description;
+          course.price = price;
+          course.company = company;
+          course.category = category;
+    
+          await course.save();
+    
+          res.json({ message: 'Course updated successfully', course });
+        } catch (error) {
+          console.error(error);
+          res.status(500).json({ message: 'Error updating the course' });
+        }
+      },
+
+
+
 }
 
 module.exports = CourseController;
--- routes/courses.js
+++ routes/courses.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const CourseController = require('../controllers/CourseController');
 
 router.post('/create', CourseController.createCourse);
+router.put('/update/:id', CourseController.updateCourse);
 
 module.exports = router;
