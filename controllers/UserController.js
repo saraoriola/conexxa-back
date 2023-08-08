7afc58a02287990d8ebbce6dd140d9b7ed3a6f33
saraoriola <saraoriola@outlook.com>
2023-08-08 11:42:51 +0200

    clean code in user controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,14 +1,19 @@
-const bcrypt = require('bcrypt'); 
-const { User, Token, Order, Course} = require('../models/index.js');
+const bcrypt = require('bcrypt');
+const { validationResult } = require('express-validator');
+const { User, Token, Order, Course } = require('../models/index.js');
 const jwt = require('jsonwebtoken');
-const { jwt_secret } = require('../config/config.json')['development']
+const { jwt_secret } = require('../config/config.json')['development'];
 
 const UserController = {
   async registerUser(req, res) {
-    
     const { name, lastName, email, password, role, company } = req.body;
 
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
       const existingUser = await User.findOne({ where: { email } });
       if (existingUser) {
         return res.status(409).json({ message: 'User already exists' });
@@ -21,8 +26,8 @@ const UserController = {
         lastName,
         email,
         password: hashedPassword,
-        role, 
-        company
+        role,
+        company,
       });
 
       res.status(201).json({ message: 'User registered successfully', user });
@@ -34,22 +39,22 @@ const UserController = {
 
   async loginUser(req, res) {
     const { email, password } = req.body;
-  
+
     try {
       const user = await User.findOne({ where: { email } });
       if (!user) {
         return res.status(404).send({ message: 'Incorrect username or password' });
       }
-  
+
       const isPasswordValid = await bcrypt.compare(password, user.password);
-  
+
       if (!isPasswordValid) {
         return res.status(401).send({ message: 'Incorrect username or password' });
       }
-  
+
       const token = jwt.sign({ id: user.id }, jwt_secret);
-      Token.create({ UserId: user.id, token }); 
-  
+      Token.create({ UserId: user.id, token });
+
       res.json({ message: 'Login successful', user, token });
     } catch (error) {
       console.error(error);
@@ -72,9 +77,9 @@ const UserController = {
     }
   },
 
-  async getUserWithOrdersAndCourses (req, res) {
+  async getUserWithOrdersAndCourses(req, res) {
     const userId = req.user.id;
-  
+
     try {
       const user = await User.findByPk(userId, {
         attributes: ['name', 'lastName', 'email', 'role'],
@@ -91,14 +96,14 @@ const UserController = {
           },
         ],
       });
-  
+
       res.json(user);
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: 'Error getting user profile' });
     }
   },
-  
+
   async updateUser(req, res) {
     const userId = req.params.id;
     const { name, lastName, email, password, role, company } = req.body;
@@ -108,7 +113,12 @@ const UserController = {
       if (!user) {
         return res.status(404).json({ message: 'User not found' });
       }
-      
+
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
       const hashedPassword = await bcrypt.hash(password, 10);
 
       user.name = name;
@@ -127,6 +137,18 @@ const UserController = {
     }
   },
 
+  async logoutUser(req, res) {
+    const userId = req.user.id;
+
+    try {
+      await Token.destroy({ where: { UserId: userId } });
+      res.json({ message: 'Logout successful' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error logging out' });
+    }
+  },
 };
 
 module.exports = UserController;
+
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const UserController = require('../controllers/UserController');
-//const { authentication, isCeo, isHR, } = require('../middleware/authentication');
+// const { authentication, isCeo, isHR, } = require('../middleware/authentication');
 const router = express.Router();
 
-
 router.post('/register', UserController.registerUser);
-router.post('/login', UserController.loginUser)
+router.post('/login', UserController.loginUser);
 router.get('/profile', UserController.getUserWithOrdersAndCourses);
-router.put('/update/:id', UserController.updateUser)
+router.put('/update/:id', UserController.updateUser);
+router.post('/logout', UserController.logoutUser);
 
 module.exports = router;
