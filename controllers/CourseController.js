ec502a42de7382b85d39ceb282658fa2bf1ec795
saraoriola <saraoriola@outlook.com>
2023-08-08 11:32:58 +0200

    Added validations in the coursess controller

--- controllers/CourseController.js
+++ controllers/CourseController.js
@@ -1,132 +1,160 @@
-const { Course } = require('../models/index.js');
+const { Course, Category } = require('../models/index.js');
 const { Op } = require('sequelize');
+const { validationResult } = require('express-validator');
 
 const CourseController = {
-    async createCourse(req, res) {
-        const { name, description, price, userId, company, categoryId } = req.body;
-    
-        try {
-          const course = await Course.create({
-            name,
-            description,
-            price,
-            userId,
-            company,
-            categoryId
-          });
-    
-          res.status(201).json({ message: 'Course created successfully', course });
-        } catch (error) {
-          console.error(error);
-          res.status(500).json({ message: 'Error creating the course' });
-        }
-      },
+  async createCourse(req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-    async updateCourse(req, res) {
-        const courseId = req.params.id;
-        const { name, description, price, company } = req.body;
-    
-        try {
-          const course = await Course.findByPk(courseId);
-          if (!course) {
-            return res.status(404).json({ message: 'Course not found' });
-          }
-    
-          course.name = name;
-          course.description = description;
-          course.price = price;
-          course.company = company;
-    
-          await course.save();
-    
-          res.json({ message: 'Course updated successfully', course });
-        } catch (error) {
-          console.error(error);
-          res.status(500).json({ message: 'Error updating the course' });
-        }
-      },
-      
-    async getCourseById(req, res) {
-        const courseId = req.params.id;
-    
-        try {
-          const course = await Course.findByPk(courseId);
-          if (!course) {
-            return res.status(404).json({ message: 'Course not found' });
-          }
-    
-          res.json(course);
-        } catch (error) {
-          console.error(error);
-          res.status(500).json({ message: 'Error getting the course' });
-        }
-      },
+    const { name, description, price, userId, company, categoryId } = req.body;
 
-    async searchCourseByName(req, res) {
-        const { name } = req.query;
-    
-        try {
-          const courses = await Course.findAll({
-            where: { name },
-          });
-    
-          res.json(courses);
-        } catch (error) {
-          console.error(error);
-          res.status(500).json({ message: 'Error searching courses' });
-        }
-      },
+    try {
+      const course = await Course.create({
+        name,
+        description,
+        price,
+        userId,
+        company,
+        categoryId,
+      });
 
-    async searchCourseByPrice(req, res) {
-        const { minPrice, maxPrice } = req.query;
-    
-        try {
-          const courses = await Course.findAll({
-            where: {
-              price: {
-                [Op.between]: [minPrice, maxPrice],
-              },
-            },
-          });
-    
-          res.json(courses);
-        } catch (error) {
-          console.error(error);
-          res.status(500).json({ message: 'Error searching courses' });
-        }
-      },
+      res.status(201).json({ message: 'Course created successfully', course });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error creating the course' });
+    }
+  },
 
-    async sortCoursesByPrice(req, res) {
-        try {
-          const courses = await Course.findAll({
-            order: [['price', 'DESC']],
-          });
-    
-          res.json(courses);
-        } catch (error) {
-          console.error(error);
-          res.status(500).json({ message: 'Error sorting courses' });
-        }
-      },
+  async updateCourse(req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-    async getAllCoursesWithCategory(req, res) {
-        try {
-          const courses = await Course.findAll({
-            include: [
-              {
-                model: Category,
-                attributes: ['id', 'name'],
-              },
-            ],
-          });
-    
-          res.json(courses);
-        } catch (error) {
-          console.error(error);
-          res.status(500).json({ message: 'Error getting courses with categories' });
-        }
-      },
-    
-}
+    const courseId = req.params.id;
+    const { name, description, price, company } = req.body;
+
+    try {
+      const course = await Course.findByPk(courseId);
+      if (!course) {
+        return res.status(404).json({ message: 'Course not found' });
+      }
+
+      course.name = name;
+      course.description = description;
+      course.price = price;
+      course.company = company;
+
+      await course.save();
+
+      res.json({ message: 'Course updated successfully', course });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error updating the course' });
+    }
+  },
+
+  async getCourseById(req, res) {
+    const courseId = req.params.id;
+
+    try {
+      const course = await Course.findByPk(courseId);
+      if (!course) {
+        return res.status(404).json({ message: 'Course not found' });
+      }
+
+      res.json(course);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error getting the course' });
+    }
+  },
+
+  async searchCourseByName(req, res) {
+    const { name } = req.query;
+
+    try {
+      const courses = await Course.findAll({
+        where: { name },
+      });
+
+      res.json(courses);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error searching courses' });
+    }
+  },
+
+  async searchCourseByPrice(req, res) {
+    const { minPrice, maxPrice } = req.query;
+
+    try {
+      const courses = await Course.findAll({
+        where: {
+          price: {
+            [Op.between]: [minPrice, maxPrice],
+          },
+        },
+      });
+
+      res.json(courses);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error searching courses' });
+    }
+  },
+
+  async sortCoursesByPrice(req, res) {
+    try {
+      const courses = await Course.findAll({
+        order: [['price', 'DESC']],
+      });
+
+      res.json(courses);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error sorting courses' });
+    }
+  },
+
+  async getAllCoursesWithCategory(req, res) {
+    try {
+      const courses = await Course.findAll({
+        include: [
+          {
+            model: Category,
+            attributes: ['id', 'name'],
+          },
+        ],
+      });
+
+      res.json(courses);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error getting courses with categories' });
+    }
+  },
+
+  async deleteCourse(req, res) {
+    const courseId = req.params.id;
+
+    try {
+      const course = await Course.findByPk(courseId);
+      if (!course) {
+        return res.status(404).json({ message: 'Course not found' });
+      }
+
+      await course.destroy();
+
+      res.json({ message: 'Course deleted successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error deleting the course' });
+    }
+  },
+};
 
 module.exports = CourseController;
--- routes/courses.js
+++ routes/courses.js
@@ -4,13 +4,11 @@ const CourseController = require('../controllers/CourseController');
 
 router.post('/create', CourseController.createCourse);
 router.put('/update/:id', CourseController.updateCourse);
+router.delete('/delete/:id', CourseController.deleteCourse);
 router.get('/courseId/:id', CourseController.getCourseById);
 router.get('/courseName/search', CourseController.searchCourseByName);
 router.get('/coursePrice/filter', CourseController.searchCourseByPrice);
 router.get('/coursePrice/sort', CourseController.sortCoursesByPrice);
-
-
-//router.delete('/courses/:id', CourseController.deleteCourse);
-//router.get('/courses', CourseController.getAllCoursesWithCategory);
+router.get('/coursesWithCategory', CourseController.getAllCoursesWithCategory);
 
 module.exports = router;
