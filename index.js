b8d11c6753a3afd5208e291459f291952a18419b
saraoriola <saraoriola@outlook.com>
2023-08-09 11:34:14 +0200

    CORS

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors'); 
 const app = express();
 const { typeError } = require('./middleware/error');
 const PORT = 3000;
 
-app.use(express.json())
+app.use(express.json());
+
+app.use(cors());
+
 app.use('/users', require('./routes/users'));
 app.use('/courses', require('./routes/courses'));
 app.use('/categories', require('./routes/categories'));
@@ -11,6 +15,5 @@ app.use('/orders', require('./routes/orders'));
 app.use('/reviews', require('./routes/reviews'));
 
 app.use(typeError);
-
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
